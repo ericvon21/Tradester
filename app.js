2c7b74c611c4cc88beaee4f3e4d85e60ab3b8a8f
Syed Mudabbir <smudabbi@purdue.edu>
2018-04-23 10:27:48 -0400

    Trading partially done

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ const passport=require('passport');
 var db = require('./tables/db');
 
 
-
 app.use(cookieSession({
 	maxAge:24*60*60*60,
 	keys:['abc']
@@ -43,6 +42,35 @@ var suggestions = ["Did you know that now you can trade items with your friends?
                     "Be on the lookout for more updates soon to come"];
 
 
+
+function noticheck(req,cb){
+    var query = "select * from item_relation where to_email=\"" +req.user.email+"\"";
+    var query2 ="SELECT * FROM mydb.users where email=";
+    var i;
+
+    db.query(query,function (err,result) {
+        if (err){
+            console.log(err);
+        }else if (result.length>0) {
+            for (i = 0; i <result.length-1; i++) {
+                query2 += "\"" + result[i].from_email + "\" or email=";
+            }
+            query2 += "\"" + result[result.length-1].from_email + "\"";
+            db.query(query2,function (err,result1) {
+                if (err){
+                    console.log(err);
+                }else{
+                    var z = JSON.parse(JSON.stringify(result1));
+                }
+                cb(result.length,z);
+            })
+        }else {
+            cb(0,null);
+        }
+    });
+
+}
+
 // create home route
 app.get('/',function (req, res) {
 	if(req.user)
@@ -57,7 +85,6 @@ app.get('/dashboard', function (req,res){
 });
 
 app.get('/profile',function(req,res){
-	console.log("profile user="+ req.user.email);
 	if(!req.user){
 		res.redirect('/');
 	}
@@ -67,11 +94,12 @@ app.get('/profile',function(req,res){
     var num_req = 0;
 
     db.query(sql_select,function(err,user_and_item){
-      //  console.log(user_and_item);
         if(err)
         {
             console.log(err);
-            res.render('Pages/newdashboard',{suggestion:suggestions[random_suggestion_num],user:req.user,feed:user_and_item});
+            noticheck(req,function(result,z){
+                res.render('Pages/newdashboard',{suggestion:suggestions[random_suggestion_num],notification:result, requser:z,user:req.user,feed:user_and_item});
+            });
             return;
         }
         db.query(sql_query,function(err,item){
@@ -81,18 +109,10 @@ app.get('/profile',function(req,res){
                 return;
             }
             num_req = item.length;
-            res.render('Pages/newdashboard',{suggestion:suggestions[random_suggestion_num],user:req.user,feed:user_and_item,pending_req :num_req});
-
+            noticheck(req,function(result,z){
+                res.render('Pages/newdashboard',{suggestion:suggestions[random_suggestion_num],notification:result, requser:z,user:req.user,feed:user_and_item,pending_req :num_req});
+            });
         });
-
-
-
-    //    if (item.length == 0) console.log(item);
-        // for(i=0;i<item.length;i++){
-        //     console.log('item name='+item[i].item_name);
-        //     console.log('item email='+item[i].email);
-        // }
-      //  res.render('Pages/newdashboard',{suggestion:suggestions[random_suggestion_num],user:req.user});
     });
 
 
@@ -100,8 +120,12 @@ app.get('/profile',function(req,res){
 });
 
 app.get('/item',function(req,res){
-   console.log(req.body.item_item);
-   res.send(req.body.trade_item);
+
+    noticheck(req,function(result,z){
+        res.render('Pages/errorpage',{user:req.user,notification:result.length, requser:z, perror:"Please Implement Trade Item first"});
+    });
+
+   //console.log(req.body.item_item);
 });
 
 
@@ -138,24 +162,73 @@ app.post('/action_page.php',function(req,res){
 });
 
 
-
 app.get('/delete_item', function(req,res){
     res.render('Pages/errorpage',{user:req.user, perror:"Please Implement Delete Item First"})
 });
 
 app.get('/trade_item',function(req,res){
-    var item_id = req.query.trade_item;
-    var item_query = 'UPDATE items SET is_trade = 1 WHERE item_id ='+item_id;
-    console.log(item_query);
+    var from_name;
+    var from_item_id = 11;
+    var from_email;
+    var from_pic_url;
+    var from_item_name;
 
-    db.query(item_query,function(err,item){
+    var to_name;
+    var to_email;
+    var to_item_id = 20;
+    var to_pic_url;
+    var to_item_name;
+
+    var from_query1 = "select * from items where item_id="+ from_item_id;
+    var from_query2 = "select * from users where email=\"";
+    var to_query1 = "select * from items where item_id="+ to_item_id;
+    var to_query2 = "select * from users where email=\"";
+
+    db.query(from_query1,function(err,from_info){
+        from_email = from_info[0].email;
+        from_pic_url = from_info[0].pic_url;
+        from_item_name = from_info[0].item_name;
+        from_query2 += from_email + "\"";
+
+        db.query(from_query2,function(err,from_info2){
+            from_name = from_info2[0].lname;
+
+            db.query(to_query1,function(err,to_info){
+                to_email = to_info[0].email;
+                to_pic_url = to_info[0].pic_url;
+                to_item_name = to_info[0].item_name;
+                to_query2 += to_email + "\"";
+
+                db.query(to_query2,function (err, to_info2) {
+                    to_name = to_info2[0].lname;
+
+                    var query = "insert into item_relation(from_itemid,to_itemid,from_email,to_email,traded,from_pic_url,to_pic_url,from_name,to_name,from_item_name,to_item_name)" +
+                        " values ("+ from_item_id + "," + to_item_id + ",\"" +from_email+"\",\""+to_email+"\",0,\"" +from_pic_url+"\",\""+to_pic_url+"\",\"" +
+                        from_name+ "\",\""+ to_name+"\",\""+from_item_name+ "\",\""+ to_item_name+"\")";
+                    console.log(query);
+                    return;
+                })
+            });
+
+
+
+
+
+
+        })
+
+    });
+
+    return;
+
+    db.query(query,function(err,item){
         if(err)
         {
             console.log('some error in item');
             console.log(err);
         }
         console.log(item);
-        res.render('Pages/errorpage',{user:req.user, perror:"Please Implement Trade Item first"});
+      //  res.render('Pages/errorpage',{user:req.user, perror:"Please Implement Trade Item first"});
     });
 
 });
@@ -169,11 +242,11 @@ app.get('/search',function(req,res){
     for (i = 0; i <tokens_size-1; i++) {
         where='item_name REGEXP\''+search_tokens[i]+'\' and ';
     }
-        where+='item_name REGEXP\''+search_tokens[tokens_size-1]+'\'';
-var sql_select='SELECT * from items where '+where;
+
+    where+='item_name REGEXP\''+search_tokens[tokens_size-1]+'\'';
+    var sql_select='SELECT * from items where '+where + ' and not email=\''+ req.user.email +' \'';
     console.log('sql search= '+sql_select);
-    //var sql_select='SELECT * from items where item_name=\''+search_query + '\'';
-  //  console.log('select=  '+sql_select);
+
     db.query(sql_select,function(err,item){
         if(err)
         {
@@ -194,26 +267,61 @@ var sql_select='SELECT * from items where '+where;
 });
 
 app.post('/items_view',function(req,res){
-    console.log(req.user);
+   // console.log(req.user);
     var sql_select='SELECT * from items where email=\''+req.user.email+'\'';
-    console.log('select=  '+sql_select);
+   // console.log('select=  '+sql_select);
     db.query(sql_select,function(err,item){
         if(err)
         {
             console.log('some error in item');
             console.log(err);
         }
-        console.log(Object.values(item));
+       // console.log(Object.values(item));
         for(i=0;i<item.length;i++){
-            console.log('item name='+item[i].item_name);
-            console.log('item email='+item[i].email);
+            //console.log('item name='+item[i].item_name);
+            //console.log('item email='+item[i].email);
         }
         if (item.length==0){
             res.render('Pages/errorpage',{user:req.user, perror:"You have no Items yet :( ...  Start by adding items!"});
             return;
         }
-        res.render('Pages/all_items',{item:item,user:req.user,search:false});
+        noticheck(req,function(result,z){
+            res.render('Pages/all_items',{item:item,user:req.user,search:false,notification:result.length, requser:z});
+        });
     });
+});
+
+app.get('/trade_page',function(req,res){
+    // var query ="select * from mydb.item_relation,mydb.items,mydb.users where item_relation.from_itemid=items.item_id and item_relation.to_email=\""+ req.user.email+"\" and  users.email=\""+ req.user.email+"\""
+    // var query1 ="select * from mydb.item_relation,mydb.items,mydb.users where item_relation.to_itemid=items.item_id and item_relation.to_email=\""+ req.user.email+"\" and  users.email=\""+ req.user.email+"\""
+
+    var query = "SELECT * FROM mydb.item_relation where to_email=\"" + req.user.email+"\"";
+    db.query(query,function(err,result1){
+       if (err){
+           console.log(err);
+       }else {
+           noticheck(req,function(result,z){
+               res.render('Pages/trade_page',{user:req.user,notification:result.length ,trade_items:result1 , requser:z});
+           });
+
+
+           // db.query(query1,function(err,result1) {
+           //     if (err){
+           //         console.log(err);
+           //     }else {
+           //
+           //         noticheck(req,function(result,z){
+           //             res.render('Pages/trade_page',{user:req.user,notification:result.length, requser:z, perror:"Please Implement Trade Item first"});
+           //         });
+           //     }
+           //
+           //
+           // });
+
+           }
+    });
+
+
 });
 
 app.get('*', function(req, res) {
---
+++ tables/item_relation.js
@@ -0,0 +1,25 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "sql123",
+    database: "mydb"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "CREATE TABLE item_relation (from_itemid int not null,to_itemid int not null" +
+        ", traded int not null,from_email VARCHAR(255) not null,to_email VARCHAR(255) not null" +
+        ",from_pic_url VARCHAR(2083) not null , to_pic_url VARCHAR(2083) not null"+
+        ",from_item_name VARCHAR(255) not null , to_item_name VARCHAR(255) not null"+
+        ",from_name VARCHAR(255) not null , to_name VARCHAR(255) not null"+
+        " , primary key (from_itemid,to_itemid)," +
+        " foreign key(from_itemid) references items(item_id)," +
+        " foreign key(to_itemid) references items(item_id))";
+    connection.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Table created");
+    });
+});
